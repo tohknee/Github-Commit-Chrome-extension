e269ca6babd3364ed3bf1e6c983c10d2d4e943eb
Tony <iamt.he@yahoo.com>
2023-12-25 16:07:11 -0800

    added chrome storage to save user input

--- popup.js
+++ popup.js
@@ -8,28 +8,59 @@ document.addEventListener('DOMContentLoaded', function () {
     const fileContentInput = document.getElementById('fileContent');
     const intervalInput = document.getElementById('interval');
     const commitCountInput = document.getElementById('commitCount');
+    const commitCounterSpan = document.getElementById('commitCounter');
   
     let commitInterval;
     let commitCount = 0;
   
-    function startCommit() {
-      commitCount = 0;
+    // Load user settings from storage
+  chrome.storage.sync.get(
+    {
+      personalToken: '',
+      owner: '',
+      repo: '',
+      commitMessage: '',
+      fileContent: '',
+      interval: 1000,
+    },
+    function (data) {
+      personalTokenInput.value = data.personalToken;
+      ownerInput.value = data.owner;
+      repoInput.value = data.repo;
+      commitMessageInput.value = data.commitMessage;
+      fileContentInput.value = data.fileContent;
+      intervalInput.value = data.interval;
+    }
+  );
+
+  function startCommit() {
+    // Save user settings to storage
+    chrome.storage.sync.set({
+      personalToken: personalTokenInput.value,
+      owner: ownerInput.value,
+      repo: repoInput.value,
+      commitMessage: commitMessageInput.value,
+      fileContent: fileContentInput.value,
+      interval: intervalInput.value,
+    });
+
+    commitCount = 0;
+    updateCommitCounter();
+
+    commitInterval = setInterval(function () {
+      commitCount++;
       updateCommitCounter();
-  
-      commitInterval = setInterval(function () {
-        commitCount++;
-        updateCommitCounter();
-        commitToGitHub();
-      }, parseInt(intervalInput.value, 10) || 1000);
-    }
-  
-    function stopCommit() {
-      clearInterval(commitInterval);
-    }
-  
-    function updateCommitCounter() {
-      commitCounterSpan.textContent = commitCount;
-    }
+      commitToGitHub();
+    }, parseInt(intervalInput.value, 10) || 1000);
+  }
+
+  function stopCommit() {
+    clearInterval(commitInterval);
+  }
+
+  function updateCommitCounter() {
+    commitCounterSpan.textContent = commitCount;
+  }
   
     function commitToGitHub() {
       const octokit = new Octokit({
